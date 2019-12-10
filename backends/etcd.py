54f5dc8fcd7f8a0854d39e638966bdcf8db83cb6
Humberto Diógenes <hdiogenes@gmail.com>
2019-12-10 00:06:42 -0300

    etcd compatibility with the fs backend api

--- backends/etcd.py
+++ backends/etcd.py
@@ -4,6 +4,26 @@ import etcd3
 
 from napps.kytos.storehouse.backends.base import StoreBase
 
+from typing import Union
+import logging
+import pickle
+
+log = logging.getLogger(__name__)
+
+
+def split_fullname(fullname: bytes):
+    """
+    >>> split_fullname(b'name.space.box_id')
+    [b'name.space', b'box_id']
+    """
+    return fullname.rsplit(b'.', maxsplit=1)
+
+
+def join_fullname(namespace: Union[bytes, str], box_id: Union[bytes, str]):
+    if isinstance(namespace, bytes) and isinstance(box_id, bytes):
+        return namespace + b'.' + box_id
+    return f'{namespace}.{box_id}'
+
 
 class Etcd(StoreBase):
     """etcd client."""
@@ -11,26 +31,34 @@ class Etcd(StoreBase):
     def __init__(self):
         self.etcd = etcd3.client()
 
+    def _get_all_keys(self):
+        return (r[1].key for r in self.etcd.get_all(keys_only=True))
+
     def create(self, box):
         """Create a new box."""
-        return self.etcd.put(f'{box.namespace}.{box.box_id}', f'{box.data}')
+        raw_data = pickle.dumps(box)
+        return self.etcd.put(join_fullname(box.namespace, box.box_id), raw_data)
 
     def retrieve(self, namespace, box_id):
         """Retrieve a box from a namespace."""
-        return self.etcd.get(f'{namespace}.{box_id}')
+        raw_data, _ = self.etcd.get(join_fullname(namespace, box_id))
+        return pickle.loads(raw_data) if raw_data else raw_data
 
     def delete(self, namespace, box_id):
         """Delete a box from a namespace."""
-        return self.etcd.delete(f'{namespace}.{box_id}')
+        return self.etcd.delete(join_fullname(namespace, box_id))
 
     def list(self, namespace):
-        """List all the boxes in a namespace."""
-        return self.etcd.get_prefix(namespace)
+        """List all the box_id's in a namespace."""
+        result = self.etcd.get_prefix(namespace, keys_only=True)
+        return (split_fullname(obj.key)[1] for (_, obj) in result)
 
     def list_namespaces(self):
         """List all the namespaces registered."""
-        return self.etcd.get_all(keys_only=True)
+        return set(split_fullname(k)[0] for k in self._get_all_keys())
 
     def backup(self, namespace=None, box_id=None):
         """Backup all the namespaces registered."""
-        return self.etcd.get_all()
+        return (pickle.loads(box) for box in self.etcd.get_all())
+
+    get = retrieve
